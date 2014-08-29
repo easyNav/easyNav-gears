32aec34c1a5f483659b3fecadbb3a2845347e091
Raaj <yaadhavraaj@gmail.com>
2014-08-29 22:18:59 +0800

    Updated Content

---
+++ CG3002_ROS_WS/devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/pi/easyNav-gears/CG3002_ROS_WS/build/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
---
+++ CG3002_ROS_WS/devel/setup.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+# generated from catkin/cmake/templates/setup.zsh.in
+
+CATKIN_SHELL=zsh
+emulate sh # emulate POSIX
+. "/home/pi/easyNav-gears/CG3002_ROS_WS/devel/setup.sh"
+emulate zsh # back to zsh mode
